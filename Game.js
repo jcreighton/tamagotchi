a02596ef9b57f29fb58c69493dbe0e3cff397a4b
Jennifer Creighton <jenncreighton@Jennifers-MacBook-Pro.local>
2018-07-01 17:00:12 -0400

    thenable coroutine

--- Game.js
+++ Game.js
@@ -6,16 +6,47 @@ var context = canvas.getContext('2d');
 class Game {
   constructor() {
     this.started = false;
+    this.userEvents = [];
+
+    this.start = this.start.bind(this);
+    this.startLoop = this.startLoop.bind(this);
+    this.isPending = this.isPending.bind(this);
+    this.loop = this.loop.bind(this);
   }
 
   start() {
     this.started = true;
-    coroutine(egg.hatch());
-    // Start loop
+    coroutine(egg.hatch())
+      .then(this.startLoop);
+  }
+
+  isPending() {
+    return this.userEvents.length;
+  }
+
+  startLoop() {
+    coroutine(this.loop());
   }
 
   loop() {
+    const {
+      isPending,
+      userEvents,
+    } = this;
 
+    return function* loop() {
+      while (true) {
+        if (isPending()) {
+          const event = userEvents.shift();
+          yield tamagotchi.reset;
+          yield event();
+        }
+
+        // Grab latest action from Tamagotchi
+        const idle = generate(tamagotchi.idle());
+        yield* idle(isPending);
+      }
+    }
   }
 }
 
@@ -28,64 +59,18 @@ var buttonA = document.querySelector('.a');
 var buttonB = document.querySelector('.b');
 var buttonC = document.querySelector('.c');
 
-var pendingActions = [];
-var time;
-var timeActionHandled;
-
-function* loop() {
-  while (game.started) {
-    if (isPending()) {
-      const action = pendingActions.shift();
-      yield tamagotchi.reset;
-      yield action();
-    }
-
-    // Grab latest action from Tamagotchi
-    const idle = generate(tamagotchi.idle());
-    yield* idle(isPending);
-  }
-}
-
-function isPending() {
-  return pendingActions.length;
-}
-
-function startLoop() {
-  if (!game.started) {
-    game.start();
-    coroutine(loop);
-  }
-
-}
-
 function feed() {
   return tamagotchi.eat();
 }
 
 buttonA.addEventListener('click', () => {
-  console.log('FEED TAMAGOTCHI!');
-  const date = new Date(Date.now());
-  time = date.getSeconds();
-  pendingActions.push(feed);
+  if (game.started) {
+    game.userEvents.push(feed);
+  }
 });
 
-buttonB.addEventListener('click', game.start);
-
-
-  // tamagotchi.moveRight()
-  //   .then(tamagotchi.eat)
-  //   .then(tamagotchi.jump);
-  // egg.bounce(3)
-  //   .then(() => {
-  //     console.log('Bouncing complete!');
-  //     console.log('Time to hatch!');
-  //     return egg.hatch();
-  //   }).then(() => {
-  //     clear();
-  //     canvas.height = tamagotchi.sprite.frameHeight;
-  //     console.log('BABY TAMAGOTCHI!')
-  //     return tamagotchi.bounce();
-  //   })
-  //   .then(tamagotchi.moveRight)
-  //   .then(tamagotchi.moveLeft)
-  //   .then(tamagotchi.bounce);
+buttonB.addEventListener('click', () => {
+  if (!game.started) {
+    game.start();
+  }
+});
--- coroutine.js
+++ coroutine.js
@@ -1,49 +1,51 @@
 const coroutine = function(generatorFunction) {
-  const generator = generatorFunction();
-  console.log(generator);
-  next();
+  return new Promise((resolve) => {
+    const generator = generatorFunction();
+    // console.log(generator);
+    next();
 
-  // Call next() or throw() on the generator as necessary
-  function next(value, isError) {
-    const response = isError ?
-      generator.throw(value) : generator.next(value);
-      console.log(generator, response);
+    // Call next() or throw() on the generator as necessary
+    function next(value, isError) {
+      const response = isError ?
+        generator.throw(value) : generator.next(value);
+        // console.log(generator, response);
 
-    if (response.done) {
-      return;
+      if (response.done) {
+        resolve(response.done);
+      }
+
+      handleAsync(response.value);
     }
 
-    handleAsync(response.value);
-  }
-
-  // Handle the result the generator yielded
-  function handleAsync(async) {
-    if (async && async.then) {
-      handlePromise(async);
-    } else if ((async && async.constructor && async.constructor.name) === 'GeneratorFunction') {
-      handleThunk(async);
-    } else if (typeof async === 'function') {
-      console.log('function');
-      var v = async();
-      next(v);
-    } else if (async === undefined) {
-      console.log('timeout');
-      setTimeout(next, 0);
-    } else {
-      next(new Error(`Invalid yield ${async}`), true);
+    // Handle the result the generator yielded
+    function handleAsync(async) {
+      if (async && async.then) {
+        handlePromise(async);
+      } else if ((async && async.constructor && async.constructor.name) === 'GeneratorFunction') {
+        handleThunk(async);
+      } else if (typeof async === 'function') {
+        // console.log('function');
+        var v = async();
+        next(v);
+      } else if (async === undefined) {
+        // console.log('timeout');
+        setTimeout(next, 0);
+      } else {
+        next(new Error(`Invalid yield ${async}`), true);
+      }
     }
-  }
 
-  // If the generator yielded a promise, call `.then()`
-  function handlePromise(async) {
-    console.log('promise');
-    async.then(next, (error) => next(error, true));
-  }
-  // If the generator yielded a thunk, call it
-  function handleThunk(async) {
-    console.log('thunk', async);
-    async((error, v) => {
-      error ? next(error, true) : next(v);
-    });
-  }
+    // If the generator yielded a promise, call `.then()`
+    function handlePromise(async) {
+      // console.log('promise');
+      async.then(next, (error) => next(error, true));
+    }
+    // If the generator yielded a thunk, call it
+    function handleThunk(async) {
+      // console.log('thunk', async);
+      async((error, v) => {
+        error ? next(error, true) : next(v);
+      });
+    }
+  });
 };
